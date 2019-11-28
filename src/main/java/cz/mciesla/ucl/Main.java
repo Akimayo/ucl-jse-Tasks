0b7ec2303e8c5adcfe9ad39b8fb593bdecb28b31
Michal Ciesla <ciesla.m@outlook.com>
2019-11-28 18:08:41 +0100

    Get rid of GUI

--- src/main/java/cz/mciesla/ucl/Main.java
+++ src/main/java/cz/mciesla/ucl/Main.java
@@ -3,16 +3,13 @@ package cz.mciesla.ucl;
 import cz.mciesla.ucl.logic.Program;
 import cz.mciesla.ucl.logic.IAppLogic;
 import cz.mciesla.ucl.ui.definition.IUserInterface;
-import cz.mciesla.ucl.ui.gui.GUI;
 import cz.mciesla.ucl.ui.cli.CLI;
 //import cz.mciesla.ucl.ui.gui.GUI; // This is for BONUS task only
 
 public class Main {
     public static void main(String[] args) {
         IAppLogic program = new Program();
-        IUserInterface ui;
-        if(args.length > 0 && args[0].equals("nogui")) ui = new CLI();
-        else ui = new GUI();
+        IUserInterface ui = new CLI();
 
         program.generateMockData();
 
